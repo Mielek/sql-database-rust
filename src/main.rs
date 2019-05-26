7d46635038cd59695993f69393f776a1dde45cff
Rafal Mielowski <mielowski.rafal@gmail.com>
2019-05-26 13:04:44 +0200

    Add recognition select and insert statement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::{stdout, stdin};
-use std::process::exit;
+use std::io::{stdin, stdout};
 use std::io::Write;
+use std::process::exit;
 
 const EXIT_SUCCESS: i32 = 0;
 
@@ -9,17 +9,50 @@ fn print_prompt() {
     stdout().flush().unwrap();
 }
 
+fn read_line() -> String {
+    print_prompt();
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Expected line of input.");
+    return input.trim().to_string();
+}
+
+fn is_meta_command(input_line: &str) -> bool {
+    return input_line.starts_with('.');
+}
+
+fn do_meta_command(input_line: &str) -> Result<(), String> {
+    match input_line {
+        ".exit" => exit(EXIT_SUCCESS),
+        _ => Err(format!("Unrecognized command '{}'.", input_line)),
+    }
+}
+
+fn prepare_statement(input_line: &str) -> Result<(), String> {
+    if input_line.starts_with("insert") {
+        return Ok(());
+    }
+    if input_line.starts_with("select") {
+        return Ok(());
+    }
+    return Err(format!("Unrecognized keyword at start of '{}'.", input_line));
+}
+
+fn execute_statement(_statement: &()) {
+    println!("Executed.");
+}
+
 fn main() {
     loop {
-        print_prompt();
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Expected line of input.");
-        let command = input.trim();
+        let input_line = read_line();
 
-        if ".exit" == command.trim() {
-            exit(EXIT_SUCCESS);
-        } else {
-            println!("Unrecognized command '{0}'.", command);
+        if is_meta_command(&input_line) {
+            do_meta_command(&input_line).err().map(|e: String| println!("{}", e));
+            continue;
+        }
+
+        match prepare_statement(&input_line) {
+            Ok(statement) => execute_statement(&statement),
+            Err(e) => println!("{}", e),
         }
     }
 }
